c608fa742656661779d21f93acf8f5a2fa691993
savamadalinmihai <savamadalinmihai@gmail.com>
2020-03-13 20:35:53 +0200

    added endpoints for all crud methods

--- src/main/java/org/fasttrackit/onlineshop/web/ProductController.java
+++ src/main/java/org/fasttrackit/onlineshop/web/ProductController.java
@@ -1,10 +1,17 @@
 package org.fasttrackit.onlineshop.web;
 
+import org.fasttrackit.onlineshop.domain.Product;
 import org.fasttrackit.onlineshop.service.ProductService;
+import org.fasttrackit.onlineshop.transfer.product.GetProductsRequest;
+import org.fasttrackit.onlineshop.transfer.product.SaveProductRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @CrossOrigin
 @RestController
@@ -17,5 +24,38 @@ public class ProductController {
     @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@Valid @RequestBody SaveProductRequest request) {
+        Product product = productService.createProduct(request);
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable long id) {
+        Product product = productService.getProduct(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Product>> getProducts(GetProductsRequest request, Pageable pageable) {
+        Page<Product> products = productService.getProducts(request, pageable);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable long id, @Valid SaveProductRequest request) {
+        Product product = productService.updateProduct(id, request);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+//    @RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
+//    the above is an older example of delete mapping annotation
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProduct(@PathVariable long id) {
+        productService.deleteProduct(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
