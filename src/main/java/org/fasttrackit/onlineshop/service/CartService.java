e643c13ad1a5f696956328a91937ad60449b758e
savamadalinmihai <savamadalinmihai@gmail.com>
2020-03-27 19:32:33 +0200

    fixed lazy loading exceptions in cart service

--- src/main/java/org/fasttrackit/onlineshop/service/CartService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/CartService.java
@@ -4,14 +4,20 @@ package org.fasttrackit.onlineshop.service;
 import org.fasttrackit.onlineshop.domain.Cart;
 import org.fasttrackit.onlineshop.domain.Customer;
 import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.persistance.CartRepository;
 import org.fasttrackit.onlineshop.transfer.cart.AddProductsToCartRequest;
+import org.fasttrackit.onlineshop.transfer.cart.CartResponse;
+import org.fasttrackit.onlineshop.transfer.cart.ProductInCartResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 @Service
 public class CartService {
@@ -29,6 +35,7 @@ public class CartService {
         this.productService = productService;
     }
 
+    // any method that connects two entities (here, cart and customer), must be annotated with @Transactional
     @Transactional
     public void addProductsToCart(AddProductsToCartRequest request){
         LOGGER.info("Adding products tot cart: {}", request);
@@ -46,5 +53,37 @@ public class CartService {
             cart.addProductToCart(product);
         }
         cartRepository.save(cart);
+    }
+
+    // returning a dto to avoid LazyLoading exceptions. For this we created a DTO with the properties needed
+    // and chose to return the DTO itself
+    @Transactional
+    public CartResponse getCart(long customerId){
+        LOGGER.info("Retrieving cart of customer {}", customerId);
+
+        Cart cart = cartRepository.findById(customerId)
+                .orElseThrow(() -> new ResourceNotFoundException("Cart " + customerId + " does not exist."));
+
+        CartResponse cartResponse = new CartResponse();
+        cartResponse.setId(cart.getId());
+
+        Set<ProductInCartResponse> productDtos = new HashSet<>();
+
+        Iterator<Product> productIterator = cart.getProducts().iterator();
+
+        while (productIterator.hasNext()){
+            Product nextProduct = productIterator.next();
+
+            ProductInCartResponse productDto = new ProductInCartResponse();
+            productDto.setId(nextProduct.getId());
+            productDto.setName(nextProduct.getName());
+            productDto.setPrice(nextProduct.getPrice());
+
+            productDtos.add(productDto);
+        }
+
+        cartResponse.setProducts(productDtos);
+
+        return cartResponse;
     }
 }
--- src/test/java/org/fasttrackit/onlineshop/CartServiceIntegrationTests.java
+++ src/test/java/org/fasttrackit/onlineshop/CartServiceIntegrationTests.java
@@ -6,11 +6,19 @@ import org.fasttrackit.onlineshop.service.CartService;
 import org.fasttrackit.onlineshop.steps.CustomerTestSteps;
 import org.fasttrackit.onlineshop.steps.ProductTestSteps;
 import org.fasttrackit.onlineshop.transfer.cart.AddProductsToCartRequest;
+import org.fasttrackit.onlineshop.transfer.cart.CartResponse;
+import org.fasttrackit.onlineshop.transfer.cart.ProductInCartResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Collections;
+import java.util.Iterator;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
 @SpringBootTest
 public class CartServiceIntegrationTests {
@@ -34,5 +42,24 @@ public class CartServiceIntegrationTests {
         cartRequest.setProductIds(Collections.singletonList(product.getId()));
 
         cartService.addProductsToCart(cartRequest);
+
+        CartResponse cart = cartService.getCart(customer.getId());
+
+        assertThat(cart, notNullValue());
+        assertThat(cart.getId(), is(customer.getId()));
+
+        assertThat(cart.getProducts(), notNullValue());
+        assertThat(cart.getProducts(), hasSize(1));
+
+        Iterator<ProductInCartResponse> productIterator = cart.getProducts().iterator();
+
+        assertThat(productIterator.hasNext(), is(true));
+
+        ProductInCartResponse nextProduct = productIterator.next();
+
+        assertThat(nextProduct, notNullValue());
+        assertThat(nextProduct.getId(), is(product.getId()));
+        assertThat(nextProduct.getName(), is(product.getName()));
+        assertThat(nextProduct.getPrice(), is(product.getPrice()));
     }
 }
